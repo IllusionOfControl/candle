d19676c9ed436e6b2d883d0167afd529dbbe8ca0
Illusion_of_control <sergeyskorokhod2@gmail.com>
2021-02-04 19:35:25 +0300

    updating models

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,22 +1,6 @@
 from django.db import models
 
 
-class Book(models.Model):
-    title = models.CharField()
-    created_at = models.DateTimeField()
-    published_at = models.DateField()
-    last_modified = models.DateTimeField()
-    uuid = models.CharField()
-    has_cover = models.BooleanField()
-    rating = models.IntegerField()
-    description = models.TextField()
-    #lang_code = models.
-
-    #publisher
-    #seria
-    #category
-
-
 class Publisher(models.Model):
     name = models.CharField()
     link = models.CharField()
@@ -25,26 +9,6 @@ class Publisher(models.Model):
 class Series(models.Model):
     name = models.CharField()
     description = models.TextField()
-
-
-class Category(models.Model):
-    name = models.CharField()
-
-    #parent_category
-
-
-class Files(models.Model):
-    pass
-
-
-class User(models.Model):
-    username = models.CharField()
-    email = models.CharField()
-    password_hash = models.CharField()
-    #secret
-    created_at = models.DateTimeField
-    last_login_at = models.DateTimeField
-    #is_admin
 
 
 class Shelf(models.Model):
@@ -59,11 +23,48 @@ class Author(models.Model):
     link = models.CharField()
 
 
-class Identifiers(models.Model):
+class Tag(models.Model):
     name = models.CharField()
 
 
-class Tags(models.Model):
+class User(models.Model):
+    username = models.CharField()
+    email = models.CharField()
+    password_hash = models.CharField()
+    is_banned = models.BooleanField()
+    created_at = models.DateTimeField()
+    last_login_at = models.DateTimeField()
+
+
+class Book(models.Model):
+    title = models.CharField()
+    created_at = models.DateTimeField()
+    published_at = models.DateField()
+    last_modified = models.DateTimeField()
+    uuid = models.CharField()
+    has_cover = models.BooleanField()
+    rating = models.IntegerField()
+    description = models.TextField()
+
+    publisher = models.ForeignKey(Publisher)
+    series = models.ForeignKey(Series)
+
+    tags = models.ManyToManyField(Tag)
+    shelves = models.ManyToManyField(Shelf)
+
+
+class File(models.Model):
+    uuid = models.CharField()
+    extension = models.CharField()
+    md5 = models.CharField()
+    size = models.IntegerField()
+
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    uploader = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Identifier(models.Model):
     name = models.CharField()
+    value = models.CharField()
 
-
+    book = models.ForeignKey(Book)
