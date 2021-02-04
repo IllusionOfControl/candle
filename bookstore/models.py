142b1d759db0c8ec82d82de54f2a69653616df12
Illusion_of_control <sergeyskorokhod2@gmail.com>
2021-02-04 15:37:51 +0300

    added models

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField()
+    created_at = models.DateTimeField()
+    published_at = models.DateField()
+    last_modified = models.DateTimeField()
+    uuid = models.CharField()
+    has_cover = models.BooleanField()
+    rating = models.IntegerField()
+    description = models.TextField()
+    #lang_code = models.
+
+    #publisher
+    #seria
+    #category
+
+
+class Publisher(models.Model):
+    name = models.CharField()
+    link = models.CharField()
+
+
+class Series(models.Model):
+    name = models.CharField()
+    description = models.TextField()
+
+
+class Category(models.Model):
+    name = models.CharField()
+
+    #parent_category
+
+
+class Files(models.Model):
+    pass
+
+
+class User(models.Model):
+    username = models.CharField()
+    email = models.CharField()
+    password_hash = models.CharField()
+    #secret
+    created_at = models.DateTimeField
+    last_login_at = models.DateTimeField
+    #is_admin
+
+
+class Shelf(models.Model):
+    name = models.CharField()
+    description = models.TextField()
+    is_public = models.BooleanField()
+
+
+class Author(models.Model):
+    name = models.CharField()
+    description = models.TextField()
+    link = models.CharField()
+
+
+class Identifiers(models.Model):
+    name = models.CharField()
+
+
+class Tags(models.Model):
+    name = models.CharField()
+
+
