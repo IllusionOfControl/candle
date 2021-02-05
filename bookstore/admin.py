f7898119f564b99df1df3a38314eb7a816b4a095
Illusion_of_control <sergeyskorokhod2@gmail.com>
2021-02-05 13:24:19 +0300

    Added models to admin panel

--- bookstore/admin.py
+++ bookstore/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from bookstore.models import *
 
-# Register your models here.
+admin.site.register(Book)
+admin.site.register(Shelf)
+admin.site.register(Series)
+admin.site.register(Publisher)
+admin.site.register(Author)
+admin.site.register(Comments)
+admin.site.register(File)
+admin.site.register(Identifier)
+admin.site.register(Tag)
