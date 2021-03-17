165d0fd76454153d35f4aa39176f711c9158a96b
Illusion_of_control <sergeyskorokhod2@gmail.com>
2021-03-17 12:47:27 +0300

    fixed all url paths

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -2,29 +2,29 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', BookListView.as_view(), name='index'),
-    path('books/add/', BookAddView.as_view(), name='book-add'),
-    path('book/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/add', BookAddView.as_view(), name='book-add'),
+    path('book/<int:pk>', BookDetailView.as_view(), name='book-detail'),
     path('book/<int:pk>/edit', BookEditView.as_view(), name='book-edit'),
 
-    path('authors/', AuthorListView.as_view(), name='author-list'),
-    path('author/<int:pk>/', AuthorDetailView.as_view(), name='author-detail'),
+    path('authors', AuthorListView.as_view(), name='author-list'),
+    path('author/<int:pk>', AuthorDetailView.as_view(), name='author-detail'),
 
-    path('tags/', TagListView.as_view(), name='tag-list'),
-    path('tag/<int:pk>/', TagDetailView.as_view(), name='tag-detail'),
+    path('tags', TagListView.as_view(), name='tag-list'),
+    path('tag/<int:pk>', TagDetailView.as_view(), name='tag-detail'),
 
-    path('series/', SeriesListView.as_view(), name='series-list'),
-    path('series/<int:pk>/', SeriesDetailView.as_view(), name='series-detail'),
+    path('series', SeriesListView.as_view(), name='series-list'),
+    path('series/<int:pk>', SeriesDetailView.as_view(), name='series-detail'),
 
-    path('publishers/', PublisherListView.as_view(), name='publisher-list'),
-    path('publisher/<int:pk>/', PublisherDetailView.as_view(), name='publisher-detail'),
+    path('publishers', PublisherListView.as_view(), name='publisher-list'),
+    path('publisher/<int:pk>', PublisherDetailView.as_view(), name='publisher-detail'),
 
     path('files/upload', FileUploadView.as_view(), name='file-upload'),
     path('file/<int:pk>', FileDownloadView.as_view(), name='file-download'),
     path('file/<int:pk>/delete', FileDeleteView.as_view(), name='file-delete'),
 
-    path('cover/<int:pk>/', BookCoverView.as_view(), name='book-cover'),
+    path('cover/<int:pk>', BookCoverView.as_view(), name='book-cover'),
 
-    path('search/', SearchView.as_view(), name='search'),
+    path('search', SearchView.as_view(), name='search'),
     path('search/<str:subject>', search_subject, name='search_by_subject'),
 ]
 
