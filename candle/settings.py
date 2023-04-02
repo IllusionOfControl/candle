e805c12990613638991ef4ad8674b1127b4e76d0
Illusion_of_control <sergeyskorokhod2@gmail.com>
2023-04-02 18:29:38 +0300

    changed the DEBUG const to env variable

--- candle/settings.py
+++ candle/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '1d*83p5o9c%&86vmtn*j4n#hf_ut9&^mo435ws20&hz3oq_q&%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', default=False)
 
 ALLOWED_HOSTS = []
 
